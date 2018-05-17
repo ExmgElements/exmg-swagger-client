005703c05b45a7236cf87270c4c69a84a19bccbd
Mark Smits <mark@exmachina.nl>
2018-05-17 15:26:38 +0200

    Migration to Polymer 3

--- README.md
+++ README.md
@@ -24,14 +24,6 @@ Please visit the [API Documentation and demo](http://ExmgElements.github.io/exmg
 
 First, make sure you have the [Polymer CLI](https://www.npmjs.com/package/polymer-cli) installed. Then run `polymer serve` to serve your element locally.
 
-## Install
-
-Install the component using bower
-
-```
-$ bower install --save ExmgElements/exmg-swagger-client
-```
-
 ## Viewing Your Element
 
 ```
---
+++ exmg-swagger-common.js
@@ -0,0 +1,60 @@
+import {dedupingMixin} from '@polymer/polymer/lib/utils/mixin.js';
+
+/**
+ * This mixin is used by exmg-swagger-client element to specify which
+ * client condig is used and when the API is ready.
+ *
+ *
+ * @polymer
+ * @memberof Exmg
+ * @mixinFunction
+ */
+const swaggerCommon = (superClass) => class extends superClass {
+  static get properties() {
+    return {
+      /**
+       * Will contain the swagger API after initialisation
+       */
+      swaggerClientApi: {
+        type: Object,
+        notify: true,
+        observer: '_swaggerClientApiChanged'
+      },
+      /**
+       * This property can be used if you would like to use multiple swagger
+       * api's. By default the api has no name.
+       */
+      apiName: {
+        type: String,
+        notify: true,
+        value: '',
+        observer: '_apiNameChanged'
+      }
+    };
+  }
+
+  _apiNameChanged(apiName) {
+    if (this.swaggerClientApi && this.swaggerClientApi.name === apiName) {
+      return;
+    }
+    try {
+      this.swaggerClientApi = Exmg.swaggerClients.api(apiName);
+    } catch (e) {
+      /* Api hasn't been initialized yet */
+      var self = this;
+      window.addEventListener('swagger-api-initialized', function onSwaggerApiInitialized(event) {
+        window.removeEventListener('swagger-api-initialized', onSwaggerApiInitialized);
+        self._apiNameChanged(self.apiName);
+      });
+    }
+  }
+
+  _swaggerClientApiChanged(api) {
+    if (api && api.name === this.apiName) {
+      return;
+    }
+    this.apiName = api ? api.name : '';
+  }
+};
+
+export const SwaggerCommonMixin = dedupingMixin(swaggerCommon);
